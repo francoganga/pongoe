1e1918a3e7c385d7103d38baa1dc4df2354ea076
fganga <fganga@unaj.edu.ar>
2024-03-26 13:44:03 -0300

    add global context.

--- pongoe.go
+++ pongoe.go
@@ -12,9 +12,11 @@ import (
 	"github.com/flosch/pongo2/v6"
 )
 
-type Templates map[string]*pongo2.Template
+type Templates struct {
+	templates map[string]*pongo2.Template
+	globals   pongo2.Context
+}
 
-// for now we dont error
 func (views Templates) Add(name string, template *pongo2.Template) {
 
 	if name == "" {
@@ -25,28 +27,30 @@ func (views Templates) Add(name string, template *pongo2.Template) {
 		return
 	}
 
-	views[name] = template
+	views.templates[name] = template
 }
 
 func (views Templates) Render(name string, w io.Writer, c pongo2.Context) error {
 
-	if _, ok := views[name]; !ok {
+	if _, ok := views.templates[name]; !ok {
 		return fmt.Errorf("template %s not found", name)
 	}
 
-	return views[name].ExecuteWriter(c, w)
+	return views.templates[name].ExecuteWriter(views.globals.Update(c), w)
+
 }
 
 func (views Templates) Dbg() {
 
-	for k := range views {
+	for k := range views.templates {
 		fmt.Printf("template=%s\n", k)
 	}
 }
 
 func LoadTemplatesFS(dir fs.FS) *Templates {
-	fmt.Println("LoadTemplatesFS")
-	templs := make(Templates)
+	templs := Templates{}
+	templs.globals = pongo2.Context{}
+	templs.templates = make(map[string]*pongo2.Template)
 
 	ts := pongo2.NewSet("main", pongo2.NewFSLoader(dir))
 
@@ -78,7 +82,10 @@ func LoadTemplatesFS(dir fs.FS) *Templates {
 }
 
 func LoadTemplates(dirpath string) *Templates {
-	templs := make(Templates)
+	templs := Templates{}
+	templs.globals = pongo2.Context{}
+
+	templs.templates = make(map[string]*pongo2.Template)
 
 	err := filepath.Walk(dirpath, func(path string, info os.FileInfo, err error) error {
 
@@ -105,5 +112,9 @@ func LoadTemplates(dirpath string) *Templates {
 	}
 
 	return &templs
+}
+
+func (views Templates) AddGlobal(key string, value any) {
+	views.globals[key] = value
 }
 
